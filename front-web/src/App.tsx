04032602a7126a6421ecd53649a65794e8858904
Henrique de Sousa Barbosa <rafahsanttos96@gmail.com>
2021-01-10 19:29:12 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Routes from './Routes';
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
